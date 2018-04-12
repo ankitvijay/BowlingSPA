343b9af29ba206f3d7a34293edf39ea78fd87394
YOUI\roym01 <roym@youi.email>
2018-04-12 15:09:41 +1000

    added selenium core driver files

---
+++ BowlingSPAWeb/BowlingSPA.UITestFramework/1.Components/Driver.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace BowlingSPA.UITestFramework._1.Components
+{
+    class Driver
+    {
+        public static IWebDriver Instance { get; set; }
+
+        public static void InitialiseChrome()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--no-sandbox");
+
+            Instance = new ChromeDriver(options);
+
+            Instance.Manage().Window.Size = new System.Drawing.Size(1280, 1024);
+            Instance.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(60);
+            TurnOnWait();
+        }
+
+        //public static void InitialiseIE()
+        //{
+        //    //InternetExplorerOptions options = new InternetExplorerOptions();
+        //    //options.AddAdditionalCapability("ignoreZomSetting", true);
+
+        //    Instance = new InternetExplorerDriver();
+
+        //    Instance.Manage().Window.Size = new System.Drawing.Size(1280, 1024);
+        //    Instance.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(60);
+        //    TurnOnWait();
+        //}
+
+        //public static void InitialiseFirefox()
+        //{
+        //    Instance = new FirefoxDriver();
+
+        //    Instance.Manage().Window.Size = new System.Drawing.Size(1280, 1024);
+        //    Instance.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(60);
+        //    TurnOnWait();
+        //}
+
+        public static void StartQuote(string environmentUrl)
+        {
+            Instance.Navigate().GoToUrl(environmentUrl);
+        }
+
+        public static void LoadPage(string environmentUrl)
+        {
+            Instance.Navigate().GoToUrl(environmentUrl);
+        }
+
+        public static void BrowserQuit(IWebDriver driver)
+        {
+            driver.Quit();
+        }
+
+        public static void TurnOnWait()
+        {
+            Instance.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+        }
+
+        public static void TurnOffWait()
+        {
+            Instance.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0);
+        }
+
+        public void ClickByClassName(string className)
+        {
+            Instance.FindElement(By.ClassName(className)).Click();
+        }
+    }
+}
---
+++ BowlingSPAWeb/BowlingSPA.UITestFramework/1.Components/WaitFor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace BowlingSPA.UITestFramework._1.Components
+{
+    class WaitFor
+    {
+        public static WebDriverWait Five(IWebDriver driver)
+        {
+            return new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+        }
+
+        public static WebDriverWait Ten(IWebDriver driver)
+        {
+            return new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        public static WebDriverWait Twenty(IWebDriver driver)
+        {
+            return new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+        }
+
+        public static WebDriverWait Thirty(IWebDriver driver)
+        {
+            return new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+        }
+
+        public static WebDriverWait Sixty(IWebDriver driver)
+        {
+            return new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+        }
+    }
+}
